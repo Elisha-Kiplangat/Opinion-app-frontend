f5bf5498161f3c1244732b3a38cb2080d9756ef8
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-11-22 23:31:41 +0300

    components

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,62 @@
+import Link from 'next/link';
+import React from 'react';
+
+interface TLink {
+  id: number;
+  title: string;
+  url: string;
+}
+
+const links : TLink[] = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/"
+  },
+  {
+    id: 2,
+    title: "About",
+    url: "/about"
+  },
+  {
+    id: 3,
+    title: "Features",
+    url: "/features"
+  },
+  {
+    id: 4,
+    title: "Contact",
+    url: "/contact"
+  }
+]
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gradient-to-r from-blue-500 to-indigo-500 px-8 py-4 w-full shadow-md">
+      <div className="mx-auto flex flex-row justify-between items-center max-w-7xl">
+        
+        <div className="text-2xl font-extrabold text-white font-sans">
+          Opinion
+        </div>
+
+        <div className="flex space-x-8 items-center">
+          <div className="flex space-x-8 text-white font-medium text-lg">
+            {links.map(link => (
+              <Link key={link.id} href={link.url}  className="hover:text-gray-300 transition duration-300">{link.title}</Link>
+          ))}
+          </div>
+          <div className="flex space-x-4">
+            <button className="bg-transparent border border-white text-white px-4 py-2 rounded-lg hover:bg-white hover:text-blue-600 transition duration-300">
+              Sign In
+            </button>
+            <button className="bg-white text-blue-600 px-4 py-2 rounded-lg hover:bg-blue-700 hover:text-white transition duration-300">
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
